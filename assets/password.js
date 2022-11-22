26a9cdcfa79e5bb415548d43fe67b5854b004145
tatefron <marianna@lineup.ua>
2022-11-22 10:26:07 +0200

    delete all

--- assets/password.js
+++
@@ -1,9 +0,0 @@
-window['Template_Password'] = () => {
-  return {
-    passwordInput: 'login',
-    mounted() {
-      initTeleport(this.$refs.modalContent);
-      Alpine.store('modals').register('password', 'modal');
-    },
-  };
-};
--- assets/sections-example-section.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.data('ThemeSection_exampleSection', () => ({
-    message: 'this is a message',
-  }))
-});
